a396baf8b32d8df907fc8061a28e2e520bb362e0
diegogluj <diego.gluj@opinaiapanel.com>
2013-08-27 22:59:12 -0300

    Do not delete original now using S3

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,11 +1,4 @@
 class Thing < ActiveRecord::Base
 	attr_accessible :description, :date, :image
   has_attached_file :image, :styles => { :thumb => "100x100>", :big => "1024x1024>" }
-  after_save :destroy_original_image
-  
-  ##
-  # Remove the original image uploaded (Keeps defined styles)
-  def destroy_original_image
-  	File.unlink(self.image.path)
-	end
 end
